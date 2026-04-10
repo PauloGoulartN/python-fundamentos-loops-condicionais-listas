79307fa7f817e9f6525008b986486573497c871c
PauloGoulart <72026575+PauloGoulartN@users.noreply.github.com>
2026-04-10 17:24:32 -0300

    Update contAteDig0.py

--- fundamentos_loops_condicionais_listas/contAteDig0.py
+++ fundamentos_loops_condicionais_listas/contAteDig0.py
@@ -1,3 +1,14 @@
+contador = 0      # contador começa em 0
+soma = 0          # acumulador começa em 0
+numero = int(input('Digite um numero: '))  # primeira leitura
+
+while numero != 0:
+    soma += numero        # acumula
+    contador += 1         # conta
+    numero = int(input('Digite um numero: '))  # lê novamente
+
+print(f'A soma dos numero é {soma}\nO contador é {contador}')
+
 # soma = 0
 # contador = 0
 
@@ -9,14 +20,3 @@
 #     contador += 1
 
 # print(f"Soma dos numeros: {soma}\nQuantidade de numeros: {contador}")
-
-contador = 0      # contador começa em 0
-soma = 0          # acumulador começa em 0
-numero = int(input('Digite um numero: '))  # primeira leitura
-
-while numero != 0:
-    soma += numero        # acumula
-    contador += 1         # conta
-    numero = int(input('Digite um numero: '))  # lê novamente
-
-print(f'A soma dos numero é {soma}\nO contador é {contador}')
